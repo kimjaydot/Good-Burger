e47ee1f30bf245469f02d7b7ae09538c80255999
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-06-18 17:36:24 -0500

    added sequelize CLI

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,48 @@
-var orm = require("../config/orm.js");
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
+
+var Burger = sequelize.define("burger", {
+  author: {
+    type: Sequelize.STRING
   },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
+  body: {
+    type: Sequelize.STRING
   },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
+  created_at: {
+    type: Sequelize.DATE
   }
-};
+}, {
+  timestamps: false
+});
 
-module.exports = burger;
+// Syncs with DB
+Burger.sync();
+
+// Makes the Chirp Model available for other files (will also create a table)
+module.exports = Burger;
+
+// var orm = require("../config/orm.js");
+
+// var burger = {
+//   all: function(cb) {
+//     orm.all("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
+//   create: function(name, cb) {
+//     orm.create("burgers", [
+//       "burger_name", "devoured"
+//     ], [
+//       name, false
+//     ], cb);
+//   },
+//   update: function(id, cb) {
+//     var condition = "id=" + id;
+//     orm.update("burgers", {
+//       devoured: true
+//     }, condition, cb);
+//   }
+// };
+
+// module.exports = burger;
