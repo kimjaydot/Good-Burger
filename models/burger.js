911856b82eff869ad64e640cd8738943c5cb97d6
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-06-18 22:28:27 -0500

    converted mysql queries to sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,48 @@
-var orm = require("../config/orm.js");
+// Dependencies
+// =============================================================
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
-  }
-};
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references my connection to the DB.
+var sequelize = require("../config/connection.js");
 
-module.exports = burger;
+// Creates a "Book" model that matches up with DB
+var Burgers = sequelize.define("burgers", {
+  burger_name: {
+    type: Sequelize.STRING
+  },
+  devoured: {
+    type: Sequelize.BOOLEAN
+  },
+});
+
+// Syncs with DB
+Burgers.sync();
+
+// Makes the Book Model available for other files (will also create a table)
+module.exports = Burgers;
+
+// var orm = require("../config/orm.js");
+
+// var burger = {
+//   all: function(cb) {
+//     orm.all("burgers", function(res) {
+//       cb(res);
+//     });
+//   },
+//   create: function(name, cb) {
+//     orm.create("burgers", [
+//       "burger_name", "devoured"
+//     ], [
+//       name, false
+//     ], cb);
+//   },
+//   update: function(id, cb) {
+//     var condition = "id=" + id;
+//     orm.update("burgers", {
+//       devoured: true
+//     }, condition, cb);
+//   }
+// };
+
+// module.exports = burger;
