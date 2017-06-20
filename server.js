70b4ff2c44eab7628d6cc3bb768bb6d7f220d2c8
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-06-20 18:29:11 -0500

    added var db

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ require("./controllers/burgersController.js")(app);
 // app.use("/create", routes);
 
 
-app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
+// app.listen(PORT, function() {
+//   console.log("Listening on port:%s", PORT);
+// });
+
+var db = require("./models");
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
