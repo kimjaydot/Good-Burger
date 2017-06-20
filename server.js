c542bc463ade10516387f8f6f8ffbf30ccbe6ff3
Kimberley Johnson <kimberleyjohnson@Kimberleys-MacBook-Pro.local>
2017-06-20 17:55:53 -0500

    commented out use

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgersController.js");(app);
 
 app.use("/", routes);
-app.use("/update", routes);
-app.use("/create", routes);
+// app.use("/update", routes);
+// app.use("/create", routes);
 
 
 app.listen(PORT, function() {
