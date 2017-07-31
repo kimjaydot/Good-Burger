5237ce7cf4f1759f984c8a190c81326e730ca62c
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-07-31 18:52:20 -0500

    removed comments that aren't being used, cleaned up code

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -18,11 +18,11 @@ module.exports = function(app) {
     // findAll returns all entries for a table when used with no options
     console.log(db.Burgers);
     db.Burgers.findAll({}).then(function(dbBurgers) {
-      // We have access to the todos as an argument inside of the callback function
-     //Use Render
+  // We have access to the todos as an argument inside of the callback function
+  //Use Render
      console.log({dbBurgers})
      res.render("index", {'burger_data': dbBurgers} );
-      // res.json(dbBurgers);
+     // res.json(dbBurgers);
     });
   });
   // POST route for saving a new todo
@@ -71,51 +71,6 @@ module.exports = function(app) {
     }).then(function(dbBurgers) {
       // res.json(dbBurgers);
        res.redirect("/");
-
     });
   });
-
 };
-
-
-
-// var express = require("express");
-
-// var router = express.Router();
-// var burgers = require("../models/burger.js");
-
-// // get route -> index
-// router.get("/", function(req, res) {
-//   res.redirect("/burgers");
-// });
-
-// router.get("/burgers", function(req, res) {
-//   // express callback response by calling burger.selectAllBurger
-//   burgers.all(function(burgerData) {
-//     // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
-//     res.render("index", { burger_data: burgerData });
-//   });
-// });
-
-// // post route -> back to index
-// router.post("/burgers/create", function(req, res) {
-//   // takes the request object using it as input for buger.addBurger
-//   burgers.create(req.body.burger_name, function(result) {
-//     // wrapper for orm.js that using MySQL insert callback will return a log to console,
-//     // render back to index with handle
-//     console.log(result);
-//     res.redirect("/");
-//   });
-// });
-
-// // put route -> back to index
-// router.put("/burgers/update", function(req, res) {
-//   burgers.update(req.body.burger_id, function(result) {
-//     // wrapper for orm.js that using MySQL update callback will return a log to console,
-//     // render back to index with handle
-//     console.log(result);
-//     res.redirect("/");
-//   });
-// });
-
-// module.exports = router;
