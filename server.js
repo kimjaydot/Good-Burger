973609994fe3089b7fb4e664b2acface49b52c38
Kimberley Johnson <kimberleyjohnson@Kimberleys-MBP.attlocal.net>
2017-07-31 18:48:47 -0500

    added comments and spacing

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+/*Packages*/
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var PORT = process.env.PORT || 3000;
 var app = express();
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 var exphbs = require("express-handlebars");
@@ -16,15 +20,6 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 require("./controllers/burgersController.js")(app);
-
-// app.use("/", routes);
-// app.use("/update", routes);
-// app.use("/create", routes);
-
-
-// app.listen(PORT, function() {
-//   console.log("Listening on port:%s", PORT);
-// });
 
 var db = require("./models");
 
